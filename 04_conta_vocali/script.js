8f6a0640cc9ff9e3daccbc3389b24406c0358e64
Jonathanmasa <jonathanmasa97@gmail.com>
2025-01-09 17:19:16 +0100

    feat[04conta-vocali]

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,9 +4,24 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function numeroVocali(word) {
+    const vocali = ['a', 'e', 'i', 'o', 'u']
+    let count = 0;
+
+    // creo ciclo per trovare le vocali
+    for (let letter of word.toLowerCase()) {
+        if (vocali.includes(letter)) {
+            count++;
+        }
+    }
+    return count;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
+const numero = numeroVocali(word);
+console.log(`numero vocali in "${word}": ${numero}`);
+
 
 
 
